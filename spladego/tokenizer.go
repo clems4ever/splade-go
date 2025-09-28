8344057e6419329489306c9f93627c6a365f4780
Clément Michaud <clement.michaud34@gmail.com>
2025-09-28 22:24:19 +0200

    add tokenizer

---
+++ spladego/tokenizer.go
@@ -0,0 +1,31 @@
+package spladego
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/sugarme/tokenizer"
+	"github.com/sugarme/tokenizer/pretrained"
+)
+
+type Tokenizer struct {
+	tk *tokenizer.Tokenizer
+}
+
+func NewTokenizer() (*Tokenizer, error) {
+	tk, err := pretrained.FromReader(bytes.NewBuffer(embeddedTokenizer))
+	if err != nil {
+		return nil, fmt.Errorf("failed to load tokenizer: %w", err)
+	}
+	return &Tokenizer{
+		tk: tk,
+	}, nil
+}
+
+func (tk *Tokenizer) Encode(s string, addSpecialTokens bool) (*tokenizer.Encoding, error) {
+	return tk.tk.EncodeSingle(s, addSpecialTokens)
+}
+
+func (tk *Tokenizer) Decode(ids []int, skipSpecialTokens bool) string {
+	return tk.tk.Decode(ids, skipSpecialTokens)
+}
